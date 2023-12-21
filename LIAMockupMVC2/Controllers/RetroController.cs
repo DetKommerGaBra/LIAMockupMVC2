aea15a876f1e40efdb353061f5e210a91a2e0425
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-12-21 02:09:37 +0100

    New demo basics with sidebar

---
+++ LIAMockupMVC2/Controllers/RetroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LIAMockupMVC2.Controllers
+{
+    public class RetroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LIAMockupMVC2/Models/ViewModel/QsInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LIAMockupMVC2.Models.ViewModel
+{
+    public class QsInfoViewModel
+    {
+        public string? QuestionText { get; set; }
+        public int? QuestionNum { get; set; }
+        public string? AnswerText { get; set; }
+        public int? AnswerNum { get; set; }
+        public string? GroupName { get; set; }
+        public string? OrgName { get; set; }
+        public string? UserName { get; set; }
+        public int? UserId { get; set; }
+    }
+}
